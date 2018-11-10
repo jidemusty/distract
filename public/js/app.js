9ccbbcaa21365572d7338dca0785257d7419960f
Mustapha Adubiagbe <jidemustapha2013@gmail.com>
2018-11-10 14:36:16 -0600

    adding a loading status

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,17 @@
 var app = new Vue({
     el: '#app',
     data: {
-        items: []
+        items: [],
+        loading: false
     },
     methods: {
         load (service) {
+            this.loading = true
+
             axios.get('/api/news/' + service)
                 .then((response) => {
                     this.items = response.data
+                    this.loading = false
                 })
         }
     },
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,16 +1,14 @@
 @extends('app')
 
 @section('content')
-    <div class="item" v-for="item in items">
+    <img src="img/loading.gif" v-if="loading" width="20" height="20" />
+
+    <div class="item" v-for="item in items" v-else>
         <h3 class="item__title">
             <a :href="`${ item.link }}`">@{{ item.title }}</a>
         </h3>
         <div class="item__service">
             @{{ item.service }}
         </div>
-    </div>
-
-    <div class="loading">
-        One moment
     </div>
 @endsection
