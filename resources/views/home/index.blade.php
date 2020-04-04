918c1eb3cd8e76b6b89585799dac471bf8ef942c
Mustapha Adubiagbe <jidemustapha2013@gmail.com>
2020-04-04 18:35:12 -0500

    (fix) removed extra curly brace on bracket

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,7 +5,7 @@
 
     <div class="item" v-for="item in items" v-else>
         <h3 class="item__title">
-            <a :href="`${{ item.link }}`">@{{ item.title }}</a>
+            <a :href="`${ item.link }`">@{{ item.title }}</a>
         </h3>
         <div class="item__service">
             @{{ item.service }}
